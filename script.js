712c031434327e0554ea0b6dd4d02249f70bd1e5
Luca Colussi <luca.colussi@gmail.com>
2024-10-20 14:42:44 +0200

    add button colors and gitHub icon

--- script.js
+++ script.js
@@ -16,8 +16,25 @@ function drawButtons() {
         let bt = document.createElement("button");
         bt.classList.add("btn");
         bt.style.width = W + "px";
-        bt.innerHTML = buttons[i];
+        if(buttons[i] === "git") {
+            bt.innerHTML = `
+                <a href="https://github.com/LucaColussi" target="_blank">
+                    <img src="/images/gitIcon.png" alt="GitHub" style="width: 40px; height: 40px;">
+                </a>
+            `;
+        } else {
+            bt.innerHTML = buttons[i];
+        }
         bt.id = buttons[i];
+
+        if (i % 5 < 3) {
+            bt.style.backgroundColor = "#2a2a2c"; // Cambia il colore di sfondo (per esempio blu chiaro)
+        }
+
+        if (i === 3 || i === 8 || i === 13) {
+            bt.style.backgroundColor = "#5c5c5f"; // Cambia il colore di sfondo (per esempio giallo oro)
+        }
+        
         cont.appendChild(bt);
     }
 }
