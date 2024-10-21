58998f27de0d962e2b728a4eaac6d6e4c47881d2
Luca Colussi <luca.colussi@gmail.com>
2024-10-21 13:07:32 +0200

    add buttons functionality and formatted expression

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+let display = document.querySelector(".display");
+let pi = document.createElement("p");
+
+
 function drawButtons() {
     let cont = document.querySelector(".buttons");
     cont.style.display = "flex";
     cont.style.flexWrap = "wrap";
     cont.style.gap = "10px";
     const W = (window.innerWidth - 240) / 5;
-
     const buttons = [
         '7', '8', '9', 'AC', '/',
         '4', '5', '6', '+/-', '*',
@@ -18,14 +21,16 @@ function drawButtons() {
         bt.style.width = W + "px";
         if(buttons[i] === "git") {
             bt.innerHTML = `
-                <a href="https://github.com/LucaColussi" target="_blank">
                     <img src="/images/gitIcon.png" alt="GitHub" style="width: 40px; height: 40px;">
-                </a>
             `;
         } else {
             bt.innerHTML = buttons[i];
         }
         bt.id = buttons[i];
+        bt.onclick = function() {
+            writeOnDisplay(buttons[i]);
+        };
+        
 
         if (i % 5 < 3) {
             bt.style.backgroundColor = "#2a2a2c"; // Cambia il colore di sfondo (per esempio blu chiaro)
@@ -39,4 +44,63 @@ function drawButtons() {
     }
 }
 
+function writeOnDisplay(value) {
+    console.log(value);
+    if(pi.textContent === "0" && value !== "+" && value !== "/" && value !== "*" && value !== "%") {
+        pi.innerHTML = ""; // Svuota il contenitore
+    }
+    if(value === "git") {
+        window.open("https://github.com/LucaColussi");
+    }    
+    else if(value === "=") {
+        evaluate(pi.textContent);
+    }
+    else if(value === "AC") {
+        erase();
+    } 
+    else if(value === "+/-"){
+        console.log("lavori in corso...");
+    }
+    else if(value === ","){
+        if(!isPresent(pi.textContent)) {
+            write(value);
+        }
+    }
+    else if(value === "+" || value === "-" || value === "/" || value === "*" || value === "%") {
+        if(!isNear(pi.textContent)){
+            write(value);
+        }
+    } 
+    else {
+        write(value);
+    }
+    display.appendChild(pi);
+}
+
+function write(text) {
+    pi.textContent += text;
+}
+
+function erase() {
+    pi.innerText = "0";
+}
+
+function isPresent(exp) {
+    if(exp.includes(",")) return true;
+    return false;
+}
+
+function isNear(exp) {
+    const operators = ['+', '-', '*', '/', '%'];
+    if(operators.includes(exp.slice(-1))) return true;
+    return false;
+}
+
+function evaluate(exp) {
+    erase();
+    console.log(exp);
+}
+
 drawButtons();
+writeOnDisplay(0);
+
